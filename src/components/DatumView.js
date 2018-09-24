f3909665d1ba077fdaffdc90cb65ee654fd9b097
kotta <opkotta@gmail.com>
2018-09-24 16:04:17 +0800

    feat: add basic layout

---
+++ src/components/DatumView.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../assets/style/DatumView.scss';
+
+export default class DatumView extends React.Component {
+  state = {
+    currentView: 1
+  };
+  render() {
+    const { currentView } = this.state;
+    const tabs = [
+      {
+        name: 'Table View',
+        id: 1
+      },
+      {
+        name: 'Graph View',
+        id: 2
+      }
+    ];
+
+    return (
+      <div className="data-view">
+        <div className="tabs">
+          {tabs.map((item, index) => (
+            <React.Fragment key={item.id}>
+              <div
+                className={'tab' + (currentView === item.id ? ' active' : '')}
+                onClick={() => this.setState({ currentView: item.id })}
+              >
+                {item.name}
+              </div>
+              {index !== tabs.length - 1 && '/'}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Pipeline.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../assets/style/Pipeline.scss';
+
+export default class Pipeline extends React.Component {
+  state = {
+    current: 1
+  };
+  render() {
+    const { current } = this.state;
+    const pipelineSteps = [
+      {
+        id: 1,
+        name: 'Attribute Initialization'
+      },
+      {
+        id: 2,
+        name: 'Data Process'
+      },
+      {
+        id: 3,
+        name: 'Result Verification'
+      }
+    ];
+    return (
+      <div className="pipeline-view">
+        <div className="pipeline-btn-group">
+          {pipelineSteps.map(item => (
+            <div
+              onClick={() => this.setState({ current: item.id })}
+              className={
+                'pipeline-button' + (current === item.id ? ' active' : '')
+              }
+              key={item.id}
+            >
+              {item.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
